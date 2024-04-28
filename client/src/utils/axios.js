ba3fccf70c0f89cfe92a8a6048739a71f22c728c
shktabish <shktab1sh93@gmail.com>
2024-04-28 08:57:14 +0530

    commit

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const api = axios.create({
     withCredentials: true,
-    baseURL: "https://kms-omega.vercel.app"
+    baseURL: "https://kms-omega.vercel.app/api/v1"
 })
 
 export default api
